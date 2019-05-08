5278e9c6352ca8f687df2c88ead909edd467614e
Unknown <kawaxbart@gmail.com>
2019-05-08 05:09:42 +0200

    Make play button works

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartNewGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Gameplay");
     }
 }
